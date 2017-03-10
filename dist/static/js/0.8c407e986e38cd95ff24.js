9667baa4bd4b787d53ac1e90e0df14d881c1cd74
babyfat <babyfat@babyfatdeMacBook-Pro.local>
2017-03-10 18:22:31 +0800

    build

--- dist/static/js/0.8c407e986e38cd95ff24.js
+++ dist/static/js/0.8c407e986e38cd95ff24.js
@@ -12457,78 +12457,52 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
   },
   mounted() {
     var self = this;
-    var payload = {
-      appId: 'wx829b884172f246ea',
-      nonceStr: __WEBPACK_IMPORTED_MODULE_1_js_md5___default()(new Date().getTime()).substring(0, 32),
-      package: 'prepay_id=' + res.body, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
-      signType: 'MD5',
-      timeStamp: new Date().getTime().toString().substring(0, 10)
-    };
-    let genSign = function () {
-      var stringA = "";
-      for (var i in payload) {
-        stringA += i + '=' + payload[i] + '&';
+    this.$http.get('/xsz/api/sign', {
+      params: {
+        url: location.href
       }
-      var stringSignTemp = stringA + "key=d41d8cd98f00b204e9800998ecf8427e";
-      console.log(stringSignTemp);
-      var sign = __WEBPACK_IMPORTED_MODULE_1_js_md5___default()(stringSignTemp).toUpperCase();
-      return sign;
-    };
-    let param = Object.assign(payload, {
-      paySign: genSign()
+    }).then(res => {
+      var data = res.data;
+      console.log('data', data);
+      wx.config({
+        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+        appId: 'wx829b884172f246ea', // 必填，公众号的唯一标识
+        timestamp: data.timestamp, // 必填，生成签名的时间戳
+        nonceStr: data.nonceStr, // 必填，生成签名的随机串
+        signature: data.signature, // 必填，签名，见附录1
+        jsApiList: ['chooseWXPay'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+      });
+      wx.ready(function () {
+        self.$http.get('/xsz/api/unifiedorder').then(res => {
+          console.log(res);
+          var payload = {
+            appId: 'wx829b884172f246ea',
+            nonceStr: __WEBPACK_IMPORTED_MODULE_1_js_md5___default()(new Date().getTime()).substring(0, 32),
+            package: 'prepay_id=' + res.body, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
+            signType: 'MD5',
+            timestamp: new Date().getTime().toString().substring(0, 10)
+          };
+          let genSign = function () {
+            var stringA = "";
+            for (var i in payload) {
+              stringA += i + '=' + payload[i] + '&';
+            }
+            var stringSignTemp = stringA + "key=d41d8cd98f00b204e9800998ecf8427e";
+            console.log(stringSignTemp);
+            var sign = __WEBPACK_IMPORTED_MODULE_1_js_md5___default()(stringSignTemp).toUpperCase();
+            return sign;
+          };
+          console.log(genSign());
+          wx.chooseWXPay(Object.assign(payload, {
+            paySign: genSign(), // 支付签名
+            success: function (res) {
+              // 支付成功后的回调函数
+              console.log('success', res);
+            }
+          }));
+        });
+      });
     });
-    WeixinJSBridge.invoke('getBrandWCPayRequest', param, function (res) {
-      alert(res);
-      if (res.err_msg == "get_brand_wcpay_request:ok") {} // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
-    });
-    // this.$http.get('/xsz/api/sign',{
-    //     params:{
-    //       url:location.href
-    //     }
-    //   }).then((res)=>{
-    //     var data = res.data
-    //     console.log('data',data)
-    //     wx.config({
-    //         debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-    //         appId: 'wx829b884172f246ea', // 必填，公众号的唯一标识
-    //         timestamp: data.timestamp, // 必填，生成签名的时间戳
-    //         nonceStr: data.nonceStr, // 必填，生成签名的随机串
-    //         signature: data.signature,// 必填，签名，见附录1
-    //         jsApiList: ['chooseWXPay'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-    //     });
-    //     wx.ready(function(){
-    //       self.$http.get('/xsz/api/unifiedorder').then((res)=>{
-    //         console.log(res)
-    //           var payload = {
-    //             appId:'wx829b884172f246ea',
-    //             nonceStr: md5(new Date().getTime()).substring(0, 32), 
-    //             package: 'prepay_id='+res.body, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
-    //             signType: 'MD5', 
-    //             timestamp: (new Date().getTime()).toString().substring(0,10)
-    //           }
-    //           let genSign = function(){  
-    //            var stringA="";
-    //            for(var i in payload){
-    //              stringA+=i+'='+payload[i]+'&'
-    //            }
-    //            var stringSignTemp=stringA+"key=d41d8cd98f00b204e9800998ecf8427e"
-    //            console.log(stringSignTemp)
-    //            var sign=md5(stringSignTemp).toUpperCase()
-    //            return sign
-    //           }
-    //           console.log(genSign())
-    //           wx.chooseWXPay(
-    //             Object.assign(payload,{
-    //               paySign: genSign(), // 支付签名
-    //               success: function (res) {
-    //                 // 支付成功后的回调函数
-    //                 console.log('success',res)
-    //               }
-    //             }))
-
-    //       })
-    //     })
-    //  })
   },
   methods: {
     pay() {}
@@ -13874,7 +13848,7 @@ exports = module.exports = __webpack_require__(14)();
 
 
 // module
-exports.push([module.i, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", "", {"version":3,"sources":[],"names":[],"mappings":"","file":"Payment.vue","sourceRoot":""}]);
+exports.push([module.i, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", "", {"version":3,"sources":[],"names":[],"mappings":"","file":"Payment.vue","sourceRoot":""}]);
 
 // exports
 
@@ -22144,4 +22118,4 @@ module.exports = function(module) {
 
 /***/ })
 ]);
-//# sourceMappingURL=0.3c0b3a8fc1cf40e4574b.js.map
+//# sourceMappingURL=0.8c407e986e38cd95ff24.js.map
