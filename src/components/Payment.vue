7e735a5d48d5a178965ed7843f967aeff767258c
babyfat <babyfat@babyfatdeMacBook-Pro.local>
2017-03-10 16:57:43 +0800

    build

--- src/components/Payment.vue
+++ src/components/Payment.vue
@@ -35,10 +35,10 @@ export default {
           self.$http.get('/xsz/api/unifiedorder').then((res)=>{
             console.log(res)
               var payload = {
-                nonceStr: '123', 
+                nonceStr: md5(new Date().getTime()).substring(0, 32), 
                 package: 'prepay_id='+res.body, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
                 signType: 'MD5', 
-                timestamp: 0
+                timestamp: new Date().getTime()
               }
               let genSign = function(){  
                var stringA="";
@@ -50,17 +50,16 @@ export default {
                var sign=md5(stringSignTemp).toUpperCase()
                return sign
               }
-              wx.chooseWXPay({
-                timestamp: 0, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
-                nonceStr: '123', // 支付签名随机串，不长于 32 位
-                package: 'prepay_id='+res.body, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
-                paySign: genSign(), // 支付签名
-                signType: 'MD5', // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
-                success: function (res) {
+              console.log(genSign())
+              wx.chooseWXPay(
+                Object.assign(payload,{
+                  paySign: genSign(), // 支付签名
+                  success: function (res) {
                     // 支付成功后的回调函数
                     console.log('success',res)
-                }
-              })
+                  }
+                }))
+              
           })
         })
      })
